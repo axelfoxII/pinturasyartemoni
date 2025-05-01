aee9904c1e93734ba528928c7009378c4f1167e7
Alex Salvatierra <asfoxii@gmail.com>
2025-04-30 23:09:40 -0400

    background  daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}"  // Incluir .ts para componentes de Angular
+  ],
   theme: {
     extend: {},
   },
   plugins: [
     require('daisyui'),
   ],
-  daisyui:{
-    themes:['night'],
+  daisyui: {
+    themes: ['night'],  // Configurar tema de daisyUI
   }
 }
 
